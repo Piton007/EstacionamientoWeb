95d036de93f794607079b9deb05e24c0b1c9c633
Jose <jose45321@outlook.com>
2019-04-15 00:30:52 -0500

    Relacione lo cruds dao a las demas interfaces

--- Data/ICajeroRepository.cs
+++ Data/ICajeroRepository.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Entidades;
 namespace Data
 {
-    interface ICajeroRepository
+    interface ICajeroRepository: ICRUDRepository<Cajero>
     {
+
     }
 }
--- Data/IComprobanteRepository.cs
+++ Data/IComprobanteRepository.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Entidades;
 namespace Data
 {
-    interface IComprobanteRepository
+    interface IComprobanteRepository: ICRUDRepository<Comprobante>
     {
     }
 }
